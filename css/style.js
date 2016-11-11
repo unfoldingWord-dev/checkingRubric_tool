850d2c0272b2e22de88b5d5167d32449a537c20f
mannycolon <colonmanuel7@gmail.com>
2016-11-11 13:48:56 -0500

    added commentbox to the view, not yet fully functional. created questionlist.json file where all questions for the 5 group/category is being saved

--- css/style.js
+++ css/style.js
@@ -1,17 +1,31 @@
 var style = {
+  toolName:{
+    margin: '5px 0 5px 20px',
+    width: '100%',
+    fontWeight: 'bold',
+    fontSize: '28px'
+  },
+
   targetChapterLayout:{
-    width: "450px",
+    width: "50%",
     height: "500px",
+    margin: "20px 0px 0px 20px",
+    padding: "35px",
     backgroundColor: "#FFF",
     border: "2px solid black",
     overflow: "scroll",
-    padding: "35px",
     textAlign: "justify",
-    margin: "20px"
+    float: "left",
   },
 
   rubricLayout:{
-    
+    width: "40%",
+    height: "500px",
+    margin: "20px 20px 0px 0px",
+    border: "2px solid black",
+    float: "left",
   },
 
 }
+
+module.exports = style;
--- subcomponents/Rubric.js
+++ subcomponents/Rubric.js
@@ -9,14 +9,22 @@ const style = require('../css/style.js');
  render all the 5 questions of the rubric QA check
  */
 
+
 class Rubric extends React.Component {
   constructor() {
     super();
     this.state = {
+      questionsList: this.getWordList()
     }
   }
 
+  getWordList() {
+     var questionsList = require('../static/QuestionsList.json').questionsList;
+     return questionsList;
+   }
+
   render() {
+    console.log(this.state.questionsList);
     return (
       <div style={style.rubricLayout}>
 
